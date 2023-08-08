7009fdce652407b4a4382a545a32820583f067ba
Divya Gupta <54738711+divyagupta167@users.noreply.github.com>
2023-08-08 22:47:56 +0530

    shopping cart functionality

---
+++ src/cart.js
@@ -0,0 +1,153 @@
+let label = document.getElementById( "label" );
+let ShoppingCart = document.getElementById( "shopping-cart" );
+
+let basket = JSON.parse( localStorage.getItem( "data" ) ) || [];
+
+console.log( basket );
+let calculation = () =>
+{
+    let cartIcon = document.getElementById( "cartAmount" );
+    cartIcon.innerHTML = basket.map( ( x ) => x.item ).reduce( ( x, y ) => x + y, 0 );
+};
+
+calculation();
+
+let generateCarditems = () =>
+{
+    if ( basket.length !== 0 )
+    {
+        // console.log("basket is not empty");
+        return ( ShoppingCart.innerHTML = basket
+            .map( ( x ) =>
+            {
+                let { id, item } = x;
+                let search = shopItemsData.find( ( e ) => e.id === id ) || [];
+                let {img,name,price}=search
+                return `
+            <div class="cart-item">
+            <img  width="100"  height="100" src="${ search.img }" >
+              <div class="details">
+                <div class="title-price-x">
+                    <h4 class="title-price">
+                        <p>${ name }</p>
+                        <p  class="cart-item-price" >$ ${ price }</p>
+                    </h4>
+                    <i onclick="removeItem(${ id })"   class="bi bi-x-lg"></i>
+                </div>
+                <div class="buttons">
+                <i onclick="decrement(${ id })" class="bi bi-dash-lg"></i>
+                <div id=${ id } class="quantity">${item} </div>
+                <i onclick="increment(${ id })" class="bi bi-plus-lg"></i>
+              </div>
+                    <h3>$ ${item* price} </h3>
+              </div>
+            </div>
+            `;
+            } )
+            .join( "" ) );
+    } else
+    {
+        // console.log("basket is not empty");
+        ShoppingCart.innerHTML = `
+        
+        `;
+        label.innerHTML = ` <h2>Cart is Empty</h2>
+        <a href="index.html">
+            <button class="HomeBtn">Back to Home</button>
+        </a>
+        `;
+    }
+};
+
+generateCarditems();
+
+let increment = ( id ) =>
+{
+  let selectedItem = id;
+
+  let search = basket.find( ( x ) => x.id === selectedItem.id );
+
+  if ( search === undefined )
+  {
+    basket.push( {
+      id: selectedItem.id,
+      item: 1,
+    } );
+  } else
+  {
+    search.item += 1;
+  }
+  update( selectedItem.id );
+  //   console.log(basket);
+  generateCarditems();
+  localStorage.setItem( "data", JSON.stringify( basket ) );
+};
+
+let decrement = ( id ) =>
+{
+  let selectedItem = id;
+
+  let search = basket.find( ( x ) => x.id === selectedItem.id );
+
+  if ( search === undefined ) return;
+  else if ( search.item === 0 )
+  {
+    return;
+  } else
+  {
+    search.item -= 1;
+  }
+  update( selectedItem.id );
+  basket = basket.filter( ( x ) => x.item != 0 );
+  // console.log(basket);
+  generateCarditems();
+  localStorage.setItem( "data", JSON.stringify( basket ) );
+};
+let update = ( id ) =>
+{
+  let search = basket.find( ( e ) => e.id === id );
+
+
+  // console.log(search.item);
+  document.getElementById( id ).innerHTML = search.item;
+  calculation();
+  TotalAmount();
+};
+
+
+let removeItem=(id)=>{
+    let selectedItem = id;
+    basket=basket.filter((x)=>x.id !== selectedItem.id);
+    generateCarditems();
+    TotalAmount();
+    calculation();
+    localStorage.setItem( "data", JSON.stringify( basket ) );
+};
+
+
+let clearcart=()=>{
+    basket=[];
+    generateCarditems();
+    calculation();
+    localStorage.setItem( "data", JSON.stringify( basket ) );
+
+}
+
+
+let TotalAmount =()=>{
+    if(basket.length !=0){
+        let amount = basket.map((x)=>{
+            let{item,id}=x;
+            let search = shopItemsData.find( ( e ) => e.id === id ) || [];
+            return item*search.price;
+        }).reduce( ( x, y ) => x + y, 0 )
+        label.innerHTML=`
+        <h2>Total Bill : $ ${amount}</h2>
+        <button class="checkout">Checkout</button>
+        <button onclick="clearcart()" class="removeAll">Clear Cart</button>
+        `
+    }else return
+}
+
+
+TotalAmount();
